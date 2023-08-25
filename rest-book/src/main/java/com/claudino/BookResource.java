31a2bcb6b6ede6a885b35951af0424b8e8995294
Claudino <brunharoto@hotmail.com>
2023-08-25 01:44:39 -0300

    Creation of Book Repository Class

--- rest-book/src/main/java/com/claudino/BookResource.java
+++ rest-book/src/main/java/com/claudino/BookResource.java
@@ -3,6 +3,9 @@ package com.claudino;
 import java.util.List;
 import java.util.Optional;
 
+import org.jboss.logging.Logger;
+
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -13,26 +16,29 @@ import jakarta.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
+    @Inject
+    BookRepository repository;
+    @Inject
+    Logger logger;
+
     @GET
     public List<Book> getAllBooks() {
-        return List.of(
-            new Book(1, "Understanding Quarkus", "Antonio", 2020, "IT"),
-            new Book(2, "Practising Quarkus", "Antonio", 2020, "IT"),
-            new Book(3, "Effective Java", "Josh Bloch", 2001, "IT"),
-            new Book(4, "Thinking in Java", "Bruce Eckel", 1998, "IT")
-        );
+        logger.info("Returns all books");
+        return repository.getAllBooks();
     }
 
     @GET
     @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
     public int countAllBooks() {
-        return getAllBooks().size();
+        logger.info("Returns the number of books");
+        return repository.getAllBooks().size();
     }
 
     @GET
     @Path("/{id}")
     public Optional<Book> getBook(@PathParam("id") int id) {
-        return getAllBooks().stream().filter(book -> book.id == id).findFirst();
+        logger.info("Returns a single book with id " + id);
+        return repository.getBook(id);
     }
 }
